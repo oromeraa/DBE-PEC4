1c2b4956d8c67668001e5a21285e9bfad897199d
Omar Romera <oromeraa@uoc.edu>
2026-01-17 23:48:37 +0100

    ejercicio 3 hecho

--- laravel-website/app/Models/Topic.php
+++ laravel-website/app/Models/Topic.php
@@ -14,9 +14,3 @@ class Topic extends Model
         return $this->belongsToMany(Museum::class);
     }
 }
-
-$guggenheim = App\Models\Museum::create(['nombre' => 'Museo Guggenheim Bilbao', 'ciudad' => 'Bilbao', 'fechas_horarios' => 'Martes a Domingo: 10:00 - 20:00', 'visitas_guiadas' => 'si', 'precio' => 16.00, 'imagen_portada' => 'museums/guggenheim.jpg']);
-
-$cosmocaixa = App\Models\Museum::create(['nombre' => 'CosmoCaixa Barcelona', 'ciudad' => 'Barcelona', 'fechas_horarios' => 'Todos los días: 10:00 - 20:00', 'visitas_guiadas' => 'si', 'precio' => 6.00, 'imagen_portada' => 'museums/cosmocaixa.jpg']);
-// Vincular con las dos temáticas
-$cosmocaixa->topics()->attach([$t1->id, $t2->id]);
